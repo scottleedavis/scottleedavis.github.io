2860e7f3ecd250402203cd2d1b007f4191387ed2
skawtus <skawtus@gmail.com>
2014-11-09 16:34:52 -0800

    added some healthcheck endpoints.

--- server.js
+++ server.js
@@ -11,6 +11,21 @@ app.use(appengine.middleware.base);
 app.listen(8080, '0.0.0.0');
 app.use("/", express.static(__dirname + '/public'));
 
+app.get('/_ah/health', function(req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send(200, 'ok');
+});
+
+app.get('/_ah/start', function(req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send(200, 'ok');
+});
+
+app.get('/_ah/stop', function(req, res) {
+  res.set('Content-Type', 'text/plain');
+  res.send(200, 'ok');
+  process.exit();
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
